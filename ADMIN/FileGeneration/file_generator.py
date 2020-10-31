19a22b3958aa297642d144c1df43624a02d7cda6
Mr Johnson Computing <djohnson@folkestonegirls.kent.sch.uk>
2020-10-31 18:41:05 +0000

    Created username generator and ran it.

--- ADMIN/FileGeneration/file_generator.py
+++ ADMIN/FileGeneration/file_generator.py
@@ -1,13 +1,51 @@
 import random
 
+PATH = "ADMIN/FileGeneration/"
+
+
 def write_file(filename, arr):
-  with open(filename, "w") as file:
+  with open(PATH + filename, "w") as file:
     for i in range(len(arr)):
       txt = str(arr[i])
       if i < len(arr) - 1:
         txt += ", "
       file.write(txt)
+
+def read_file(filename, delimiter = "\n"):
+  with open(PATH + filename, "r") as file:
+    data = file.read()
+  return data.split(delimiter)
   
+def get_male_name():
+  name = random.choice(read_file("male-names.txt"))
+  return name.capitalize()
+
+def get_female_name():
+  name = random.choice(read_file("female-names.txt"))
+  return name.capitalize()
+
+def get_surname():
+  surname = random.choice(read_file("family-names.txt"))
+  return surname.capitalize()
+
+def create_full_name():
+  gender = random.randint(1,2)
+  name = ""
+  if gender == 1:
+    name = get_male_name()
+  else:
+    name = get_female_name()
+  return name + " " + get_surname()
+
+def create_user_data():
+  data = []
+  dataset = 1000
+  for i in range(dataset):
+    name = create_full_name()
+    birth_year = random.randint(1950, 2000)
+    data.append(name + " " + str(birth_year))
+  write_file("EmployeeData.txt", data)
+
 
 def create_speed_data(size=10):
   data = []
@@ -44,3 +82,4 @@ print("Starting speed_camera/file_generator.py")
 print("File Writing Turned Off - No action was performed")
 #write_file("speed_camera/data.txt", create_data(10000))
 print("Ending speed_camera/file_generator.py")
+create_user_data()
